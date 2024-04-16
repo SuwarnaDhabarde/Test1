2f2b29ca77c70627794a95da351c7fa1cf8e5d33
Desktop <Desktop@DESKTOP-024>
2024-04-16 17:32:25 +0530

    First commit

---
+++ SampleCode/src/SampleDemo/myfirstcode.java
@@ -0,0 +1,17 @@
+package SampleDemo;
+
+public class myfirstcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		
+		int a=8, b=5;
+		int Sum;
+		Sum = a+b;
+		System.out.println(Sum);
+		if(a>b)
+		{}
+	}
+
+}
